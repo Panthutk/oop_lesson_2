a1b4e00f3f2fdeda32632afeed2cd304aeee5112
Panthut Ketphan <panthat.k@gmail.com>
2023-11-09 10:37:21 +0700

    done combination_gen.py

--- combination_gen.py
+++ combination_gen.py
@@ -1,13 +1,18 @@
+from itertools import product
+
+
 def gen_comb_list(list_set):
-    '''
-        Parameters:
-            list_set: a list of lists where each contains at least one element
 
-        Returns:
-            a list of lists, each of which is made from a combination of elements in each list in list_set
+    # Use itertools.product to get the Cartesian product of the input lists
+    combinations = list(product(*list_set))
 
-        Examples:
-            gen_comb_list([[1, 2, 3]]) returns [[1], [2], [3]]
-            gen_comb_list([[1, 2, 3], [4, 5]]) returns [[1, 4], [2, 4], [3, 4], [1, 5], [2, 5], [3, 5]]
-            gen_comb_list([[1, 2, 3], [4, 5], [6, 7, 8]]) returns [[1, 4, 6], [2, 4, 6], [3, 4, 6], [1, 5, 6], [2, 5, 6], [3, 5, 6], [1, 4, 7], [2, 4, 7], [3, 4, 7], [1, 5, 7], [2, 5, 7], [3, 5, 7], [1, 4, 8], [2, 4, 8], [3, 4, 8], [1, 5, 8], [2, 5, 8], [3, 5, 8]]
-    '''
+    # Convert each tuple in the combinations to a list
+    result = [list(combination) for combination in combinations]
+
+    return result
+
+
+# Examples
+print(gen_comb_list([[1, 2, 3]]))
+print(gen_comb_list([[1, 2, 3], [4, 5]]))
+print(gen_comb_list([[1, 2, 3], [4, 5], [6, 7, 8]]))
